b4662b3f82a1b91ebc83e4b95f82d5a8377f251b
Sonal Singh <sonal.singh.19993@gmail.com>
2020-08-03 11:55:35 +0200

    [BLOBS] sm6250-common: import MIUI power-off charging daemon
    - from qssi-user 11 RKQ1.200826.002 V12.0.1.0.RJWINXM release-keys
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- sm6250-common-vendor.mk
+++ sm6250-common-vendor.mk
@@ -18,6 +18,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/sm6250-common
 
 PRODUCT_COPY_FILES += \
+    vendor/xiaomi/sm6250-common/proprietary/bin/charger:$(TARGET_COPY_OUT_SYSTEM)/bin/charger \
     vendor/xiaomi/sm6250-common/proprietary/etc/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     vendor/xiaomi/sm6250-common/proprietary/etc/sysconfig/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     vendor/xiaomi/sm6250-common/proprietary/product/etc/permissions/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml \
